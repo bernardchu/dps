799daf762d09a5ea59a3944477e4c68adff35384
Bernard Chu <bernard.chu@yahoo.com>
2016-09-17 18:02:14 -0700

    Style adoption details text.

--- events.js
+++ events.js
@@ -1,5 +1,7 @@
 $( document ).ready( function() {
-  var eventsApi = 'http://dpsrescue.org/api/dates.php'
+  var eventsApi = 'http://dpsrescue.org/api/dates.php';
+  var noEventsFoundNode = '<h4 class="event">No upcoming events - stay tuned for updates!</h4>'
+
   $.get( eventsApi )
     .then( function( response ) {
       var events = _.flatten( response );
@@ -9,6 +11,9 @@ $( document ).ready( function() {
       eventNodes.forEach( function ( node ) {
         $( '.dates' ).append( node );
       } );
+      if (!eventNodes.length) {
+        $( '.dates' ).append( noEventsFoundNode );
+      }
     } );
 } );
 
