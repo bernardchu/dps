b31ef34d0d4986dca0a952ccb2537b2a7c312f7e
Bernard Chu <bernard.chu@yahoo.com>
2016-09-18 15:40:07 -0700

    Add all dogs view.

---
+++ adopt/available/available.js
@@ -0,0 +1,43 @@
+var availableUri = 'http://api.dpsrescue.com/api/available.php';
+
+var available = new Available();
+
+function Available () {
+  this.facets = {};
+  this.dogs = [];
+}
+
+Available.prototype.getFacets = function () {
+  return Object.keys( this.facets );
+}
+
+Available.prototype.getFacetValues = function ( facet ) {
+  if (!this.facets[facet]) { return [] }
+  return Object.keys( this.facets[facet] );
+}
+
+Available.prototype.createAnimalNode = function ( dog ) {
+  return '<a href="../pet-detail?id=' + dog.id + '" class="available">'
+          +'<div class="img-container">'
+            +'<img src="' + dog.pictures[0].thumb + '">'
+          +'</div>'
+          +'<div class="name">' + dog.name + '</div>'
+          +'<div class="breed">' + dog.breed + '</div>'
+          +'<div class="age">' + dog.age + ' - ' + dog.gender + '</div>'
+        +'</a>'
+}
+
+Available.prototype.populateDogs = function ( node ) {
+  this.dogs.forEach( function ( dog ) {
+    node.append( available.createAnimalNode( dog ) );
+  } )
+}
+
+$( document ).ready( function() {
+  $.get( availableUri )
+    .then( function ( response ) {
+      available.dogs = response.dogs.animals;
+      available.facets = response.facets;
+      available.populateDogs( $( 'div.list' ) );
+    } );
+} );
