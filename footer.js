265220dfcf17d2aa90209121eddc6bb358528f95
Bernard Chu <bernard.chu@yahoo.com>
2016-09-18 09:35:57 -0700

    Break up styles.

--- footer.js
+++ footer.js
@@ -2,13 +2,13 @@ $( document ).ready( function() {
   var footer = '<div class="footer">' +
       '<div class="social-icons">' +
         '<a href="https://twitter.com/dpsrescue" target="_blank" rel="noopener">' +
-          '<img src="./images/twitter_icon.png" width="56" height="63">' +
+          '<img src="' + window.location.origin + '/images/twitter_icon.png" width="56" height="63">' +
         '</a>' +
         '<a href="https://facebook.com/dpsrescue" target="_blank" rel="noopener">' +
-          '<img src="./images/facebook_icon.png" width="56" height="63">' +
+          '<img src="' + window.location.origin + '/images/facebook_icon.png" width="56" height="63">' +
         '</a>' +
         '<a href="https://instagram.com/dpsrescue" target="_blank" rel="noopener">' +
-          '<img src="./images/instagram_icon.png" width="56" height="63">' +
+          '<img src="' + window.location.origin + '/images/instagram_icon.png" width="56" height="63">' +
         '</a>' +
       '</div>' +
         'Copyright 2016 Doggie Protective Services | PO Box 1508 • Romoland, CA 92585' +
