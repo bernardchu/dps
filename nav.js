11d035165c0115823c805892fb66f6ca3a6c3eb5
Bernard Chu <bernard.chu@yahoo.com>
2016-09-18 09:54:41 -0700

    Highlight active states in nav.

--- nav.js
+++ nav.js
@@ -38,7 +38,7 @@ var states = [
     ]
   },
   { name: 'Donate',
-    route: '',
+    route: '/donate',
     children: [
       {
         name: 'Wishlist',
@@ -84,8 +84,8 @@ $( 'document' ).ready( function () {
 
 function Nav (states) {
   this.states = states;
-  this.mastheadNode = $( '<a class="masthead" href="' + window.location.host + '"></a>' );
-  this.logoNode = $( '<a href="" class="logo"><img src="./images/logo.png" alt="DPS Logo" /></a>' );
+  this.mastheadNode = $( '<a class="masthead" href="' + window.location.origin + '"></a>' );
+  this.logoNode = $( '<a href="" class="logo"><img src="' + window.location.origin + '/images/logo.png" alt="DPS Logo" /></a>' );
   this.mastheadNode.append( this.logoNode );
   this.navNode = $( '<span class="nav"></span>' );
   this.mastheadNode.append( this.navNode );
@@ -101,19 +101,57 @@ Nav.prototype.getRoute = function ( state ) {
 Nav.prototype.buildNav = function () {
   var _this = this;
   this.states.forEach( function buildTopNav ( state ) {
-    var stateNode = $( '<li><a class="primary-link" href="' + _this.getRoute( state ) + '">' + state.name + '</a></li>' );
-    if (state.children.length) {
-      var subNavNode = $( '<ul class="subnav"></ul>')
-      state.children.forEach( function ( child ) {
-        var childNode = $( '<li><a href="' + state.route + child.route + '">' + child.name + '</a></li>' );
-        subNavNode.append( childNode );
-      } )
-      stateNode.append( subNavNode );
-    }
-    _this.navNode.append( stateNode );
+    _this.navNode.append( _this.buildTopStateNode( state ) );
   } )
   return this.mastheadNode;
 }
+
+Nav.prototype.buildTopStateNode = function ( state ) {
+  var _this = this;
+  
+  var node = $( '<li><a class="primary-link clickable" href="' + _this.getRoute( state ) + '">' + state.name + '</a></li>' );
+    if (_this.isStateActive( state )) {
+      node.find( 'a' ).addClass( 'active' );
+    }
+    if (state.children.length) {
+      var subNavNode = $( '<ul class="subnav"></ul>')
+      state.children.forEach( function ( child ) {
+        var childNode = _this.buildChildStateNode( state, child );        
+        subNavNode.append( childNode );
+      } )
+      node.append( subNavNode );
+    }
+  return node;
+}
+
+Nav.prototype.isStateActive = function ( parent, child ) {
+  var parentActive = !!(new RegExp( this.getParentPath() )).test( parent.route );
+  if (!child) { return parentActive }
+  return parentActive && new RegExp( this.getChildPath() ).test( child.route );;
+}
+
+Nav.prototype.getStatePaths = function () {
+  return window.location.pathname.split( '/' ).filter( function ( part ) {
+    return !!part;
+  } )
+}
+
+Nav.prototype.getParentPath = function () {
+  return this.getStatePaths()[0];
+}
+
+Nav.prototype.getChildPath = function () {
+  return this.getStatePaths()[1];
+}
+Nav.prototype.buildChildStateNode = function ( parent, child ) {
+  var _this = this;
+  var node = $( '<li><a href="' + parent.route + child.route + '">' + child.name + '</a></li>' );
+  if (_this.isStateActive( parent, child )) {
+    node.find( 'a' ).addClass( 'active' );
+  }
+  return node;
+}
+
 
 /*
 <div class="masthead">
