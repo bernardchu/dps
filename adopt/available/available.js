6e043f14ca91c7f5592e5a9abfba5fe22a74accd
Bernard Chu <bernard.chu@yahoo.com>
2016-09-18 18:46:46 -0700

    Add (non-functional) filter radio buttons.

--- adopt/available/available.js
+++ adopt/available/available.js
@@ -5,6 +5,7 @@ var available = new Available();
 function Available () {
   this.facets = {};
   this.dogs = [];
+  this.query = {};
 }
 
 Available.prototype.getFacets = function () {
@@ -27,9 +28,9 @@ Available.prototype.createAnimalNode = function ( dog ) {
         +'</a>'
 }
 
-Available.prototype.populateDogs = function ( node ) {
+Available.prototype.populateDogs = function ( targetNode ) {
   this.dogs.forEach( function ( dog ) {
-    node.append( available.createAnimalNode( dog ) );
+    targetNode.append( available.createAnimalNode( dog ) );
   } )
 }
 
@@ -40,7 +41,35 @@ Available.prototype.toggleFilterPane = function () {
   } else {
     filterNode.addClass( 'hidden' );
   }
-  
+}
+
+Available.prototype.populateFilters = function ( targetNode ) {
+  var _this = this;
+  this.getFacets().forEach( function ( facet ) {
+    targetNode.append( _this.createFilterNode( facet, _this.facets[ facet ] ) );
+  } )
+}
+
+Available.prototype.createFilterNode = function ( label, facet ) {
+  var _this = this;
+  var node = $( '<div class="facet">' + this.facetToLabel( label ) + '</div>' );
+  Object.keys( facet ).forEach( function ( value ) {
+    _this.addRadio( node, value, label );
+  } )
+  return node;
+}
+
+// convert primary_breed to primary breed and CSS text-transform: capitalize will take care of the rest.
+Available.prototype.facetToLabel = function ( rawLabel ) {
+  return rawLabel.replace( '_', ' ' );
+}
+
+Available.prototype.labelToFacet = function ( label ) {
+  return label.replace( ' ', '_' ).toLowerCase();
+}
+
+Available.prototype.addRadio = function ( targetNode, value, label ) {
+  targetNode.append( '<label for="' + label + '">' + value + '</label><input type="radio" value="' + value + '" name="' + label + '"/>' );
 }
 
 $( document ).ready( function() {
@@ -49,7 +78,8 @@ $( document ).ready( function() {
   $.get( availableUri )
     .then( function ( response ) {
       available.dogs = response.dogs.animals;
-      available.facets = response.facets;
+      available.facets = response.dogs.facets;
       available.populateDogs( $( 'div.list' ) );
+      available.populateFilters( $( '.filter .filters' ) );
     } );
 } );
