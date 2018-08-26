af011ab597463e4c76b596e012831ab48dcf0c25
bernardchu <bernardchu@gmail.com>
2018-08-26 15:12:15 -0700

    Add Events rough draft.

--- events.js
+++ events.js
@@ -1,44 +1,81 @@
 $( document ).ready( function() {
-  var eventsApi = 'http://api.dpsrescue.com/api/dates.php';
-  var noEventsFoundNode = '<h4 class="event">No upcoming events - stay tuned for updates!</h4>'
+  var eventsApi = 'http://roastonbone.com/dps-test/dps-api/api/dates.php';
+  // var eventsApi = 'http://api.dpsrescue.com/api/dates.php';
+  var noEventsFoundNode = '<h4 class="event">No upcoming events - stay tuned for updates!</h4>';
 
   $.get( eventsApi )
-    .then( function( events ) {
-      var eventNodes = events.map( function ( event ) {
-        return createEventNode( event );
+    .then( function( locations ) {
+      var locationNodes = Object.keys(locations).map( function ( location ) {
+        return createLocationNode(locations[location]);
       } );
-      eventNodes.forEach( function ( node ) {
+      locationNodes.forEach( function ( node ) {
         $( '.dates' ).append( node );
       } );
-      if (!eventNodes.length) {
+      if (!locationNodes.length) {
         $( '.dates' ).append( noEventsFoundNode );
       }
     } );
 } );
 
-function createEventNode (event) {
-  return $( '<div class="event">'
-             +'<div class="event-date">'
-               +'<div class="event-day-of-month">' + event.day_of_month + '</div>'
-               +'<div class="event-month"><em>' + event.month + '</em></div>'
-             +'</div>'
-             +'<div class="event-details">'
-               +'<em class="event-day-of-week">' + event.day_of_week + '</em><br/>'
-               +'<em class="event-time">' + event.time + '</em>'
-               +'<a href="' + event.location_map + '" target="_blank">'
-                 +'<div class="event-location">' + event.location_name + '</div>'
-                 +'<div class="event-address">' + event.location_address + '</div>'
-               +'</a>'
-             +'</div>'
-           +'</div>');
+function createLocationNode(location) {
+  var locationNode = $( '<div class="event-location">' +
+      '<h2>' + location.name + '</h2>' +
+      '<a href="location.map_url">Location and directions</a>' +
+    '</div>' );
+  location.events && location.events.forEach( function (event) {
+    locationNode.append(createEventNode(event));
+  });
+  return locationNode;
 }
 
-/* Example event
-day_of_month:"24"
-day_of_week:"Saturday"
-location_address:"3910 Middlefield Road"
-location_map:"http://maps.google.com/maps?daddr=3910%20Middlefield%20Road,Palo%20Alto%20,CA,94303"
-location_name:"Palo Alto Pet Food Express"
-month:"September"
-time:"12:00pm - 3:00pm"
-*/
+function createEventNode (event) {
+  var eventNode = $( '<div class="event">'
+             +'<div class="event-date">' + event.date + '</div>'
+             +'<div class="event-time">' + event.time + '</div>'
+           +'</div>');
+  if (event.note) {
+    eventNode.append($('<div class="event-note">' + event.note + '</div>'));
+  }
+  return eventNode;
+}
+
+/**
+ * Example API response:
+ * {
+   "palo alto":{
+      "name":"Palo Alto Pet Food Express",
+      "map_url":"http:\/\/petfoodexpress.com\/stores\/detail\/palo-alto",
+      "events":[
+         {
+            "date":"Sunday, August 26th",
+            "time":"11:00am - 3:00pm",
+            "note":"Please note we will not start adoption interviews after 2:30pm"
+         }
+      ]
+   },
+   "redwood city":{
+      "name":"Redwood City Pet Food Express @ Woodside Plaza",
+      "map_url":"http:\/\/maps.google.com\/maps?daddr=372%20Woodside%20Plaza,Redwood%20City,CA,94061",
+      "events":[
+         {
+            "date":"Sunday, September 2nd",
+            "time":"12:00pm - 4:00pm"
+         }
+      ]
+   },
+   "adoptathon":{
+      "name":"Bay Area Pet Fair",
+      "map_url":"http:\/\/www.bayareapetfair.org\/contact-us",
+      "events":[
+         {
+            "date":"Saturday, September 15th",
+            "time":"10:00am - 5:00pm"
+         },
+         {
+            "date":"Sunday, September 16th",
+            "time":"10:00am - 5:00pm"
+         }
+      ]
+   }
+}
+ */
