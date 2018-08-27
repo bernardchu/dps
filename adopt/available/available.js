ecfc401ec8ad178b0883315b3951a0d333ccdd10
bernardchu <bernardchu@gmail.com>
2018-08-26 19:00:46 -0700

    Rough draft of Available page.

--- adopt/available/available.js
+++ adopt/available/available.js
@@ -1,118 +1,41 @@
-var availableUri = 'http://api.dpsrescue.com/api/available.php';
+var availableUri = 'http://roastonbone.com/dps-test/dps-api/api/available.php?view=compact';
+// var availableUri = 'http://api.dpsrescue.com/api/available.php';
 
 var available = new Available();
 
+$( document ).ready( function() {
+  available.initialize();
+} );
+
 function Available () {
-  this.facets = {};
   this.dogs = [];
   this.cats = [];
-  this.animal = 'dogs';
-}
-
-Available.prototype.getFacets = function () {
-  return Object.keys( this.facets );
-}
-
-Available.prototype.getFacetValues = function ( facet ) {
-  if (!this.facets[facet]) { return [] }
-  return Object.keys( this.facets[facet] );
-}
-
-Available.prototype.createAnimalNode = function ( dog ) {
-  return '<a href="../pet-detail?id=' + dog.id + '" class="available nonstandard-link">'
-          +'<div class="img-container">'
-            +'<img src="' + dog.pictures[0].img + '">'
-          +'</div>'
-          +'<div class="name">' + dog.name + '</div>'
-          +'<div class="breed">' + dog.breed + '</div>'
-          +'<div class="age">' + dog.age + ' - ' + dog.gender + '</div>'
-        +'</a>'
-}
-
-Available.prototype.populateAnimal = function ( animal, targetNode ) {
-  targetNode.html( '' );
-  this[ animal ].forEach( function ( animal ) {
-    targetNode.append( available.createAnimalNode( animal ) );
-  } )
-}
-
-Available.prototype.toggleFilterPane = function () {
-  var filterNode = $( '.filter' );
-  if ( filterNode.hasClass( 'hidden' ) ) {
-    filterNode.removeClass( 'hidden' );
-  } else {
-    filterNode.addClass( 'hidden' );
-  }
-}
-
-Available.prototype.populateFilters = function ( targetNode ) {
-  var _this = this;
-  this.getFacets().forEach( function ( facet ) {
-    targetNode.append( _this.createFilterNode( facet, _this.facets[ facet ] ) );
-  } )
-}
-
-Available.prototype.createFilterNode = function ( label, facet ) {
-  var _this = this;
-  var node = $( '<div class="facet"><h4 class="facet-heading">' + this.facetToLabel( label ) + '</h6></div>' );
-  Object.keys( facet ).forEach( function ( value ) {
-    _this.addRadio( node, value, label );
-  } )
-  return node;
-}
-
-// convert primary_breed to primary breed and CSS text-transform: capitalize will take care of the rest.
-Available.prototype.facetToLabel = function ( rawLabel ) {
-  return rawLabel.replace( '_', ' ' );
-}
-
-Available.prototype.labelToFacet = function ( label ) {
-  return label.replace( ' ', '_' ).toLowerCase();
-}
-
-Available.prototype.addRadio = function ( targetNode, value, label ) {
-  targetNode.append( '<label><input type="radio" value="' + value + '" name="' + label + '"/> ' + value + '</label>' );
-}
-
-// currently doesn't work right; will need some refactoring to handle cats and dogs
-Available.prototype.filter = function () {
-  var query = $( 'form' ).serialize();
-  if (query) {
-    query += '&type=' + this.animal;
-  }
-  this.query( query );
-  return false;
-}
-
-Available.prototype.query = function ( query ) {
-  
 }
 
 Available.prototype.initialize = function () {
+  var _this = this;
   return $.get( availableUri )
     .then( function ( response ) {
-      available.dogs = response.dogs.animals;
-      available.cats = response.cats.animals;
-      available.populateAnimal( 'dogs', $( 'div.list' ) );
-
-      available.facets = response.dogs.facets;
-      available.populateFilters( $( '.filter .filters' ) );
+      _this.dogs = response.dogs;
+      _this.cats = response.cats;
+      _this.populateAnimal( 'dogs', $( '.dogs' ) );
+      _this.populateAnimal( 'cats', $( '.cats' ) );
     } );
-}
+};
 
-Available.prototype.switchTo = function ( animal ) {
-  if (this.animal === animal) { return }
-  this.populateAnimal( animal, $( 'div.list' ) );
-  this.animal = animal;
-  $( '.available-dogs' ).toggleClass( 'active' );
-  $( '.available-cats' ).toggleClass( 'active' );
-}
+Available.prototype.populateAnimal = function ( animal, targetNode ) {
+  this[ animal ].forEach( function ( animal ) {
+    targetNode.append( available.createAnimalNode( animal ) );
+  } )
+};
 
-$( document ).ready( function() {
-  $( '.dogs-cats button' ).click( available.toggleFilterPane );
-  $( '.close-filter' ).click( available.toggleFilterPane );
-  $( '.available-dogs' ).click( available.switchTo.bind( available, 'dogs' ) );
-  $( '.available-cats' ).click( available.switchTo.bind( available, 'cats' ) );
-  $( 'form' ).submit( available.filter.bind( available ) );
-  available.initialize();
-} );
+Available.prototype.createAnimalNode = function ( animal ) {
+  return '<span class="available col-md-2 col-sm-4 col-xs-12">'
+          +'<a href="../pet-detail?id=' + animal.id + '" class="img-container">'
+            +'<img src="' + animal.imgurl + '">'
+          +'</a>'
+          +'<div class="name">' + animal.name + '</div>'
+          +'<div class="breed">' + animal.breed + '</div>'
+          +'<div class="age">' + animal.age + ' - ' + animal.gender + '</div>'
+        +'</span>'
+};
