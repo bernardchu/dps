57a901da899a713fa0f90173c2f79ed88ad41241
bernardchu <bernardchu@gmail.com>
2018-09-02 14:30:44 -0700

    Add About FAQ.

---
+++ about/faq/faq.js
@@ -0,0 +1,47 @@
+var faq = {
+  sections: [
+    {
+      heading: "Where does DPS get these dogs?",
+      imgUrl: "http://dps-festive.imgix.net/images/about/faq/sarah-chen.jpg?h=340&fit=crop&w=340",
+      prose: [
+        '<p>The majority of our dogs come from high kill animal shelters in Southern CA. The shelters there are overflowing with highly adoptable dogs that are euthanized every day. We do on occasion take our dogs from owners who are unable to continue caring for their dogs.</p>',
+        '<p>Our Korean and Chinese Rescue partnerships also allow us to save dogs around the world, which we are thankful for.</p>'
+      ]
+    },
+    {
+      heading: "Where is the shelter/kennel?",
+      imgUrl: "http://dps-festive.imgix.net/images/about/faq/chen.jpg?h=340&fit=crop&w=340&crop=top",
+      prose: [
+        "<p>We are not a shelter or a kennel. Our dogs are rescued from high kill shelters and then go into a network of foster homes in Northern CA, Southern CA and Las Vegas. Each dog lives with a family or person until they find their permanent home.</p>"
+      ]
+    },
+    {
+      heading: "Why do the dogs come from Southern CA?",
+      imgUrl: "http://dps-festive.imgix.net/images/about/faq/george.png?h=340&fit=crop&w=340",
+      prose: [
+        "<p>They stay with our rescue until they too find their home. You can check out some of our dogs who have been with us longer than others under our Sticky Dog section.</p>"
+      ]
+    },
+    {
+      heading: "What happens to the dogs that don't get adopted?",
+      imgUrl: "http://dps-festive.imgix.net/images/about/faq/zara.jpg?h=340&fit=crop&w=340&crop=top",
+      prose: [
+        "<p>They stay with our rescue until they too find their home. You can check out some of our dogs who have been with us longer than others under our Sticky Dog section.</p>",
+      ]
+    },
+    {
+      heading: "Do you have a \"trial period\" if I take a dog home?",
+      imgUrl: "http://dps-festive.imgix.net/images/about/faq/karen.jpg?h=340&fit=crop&w=340&crop=top",
+      prose: [
+        "<p>As a rule, we do not have trial periods. If you take a dog home on a trial basis for a week or two and do not end up adopting, we have not been able to use that time to find that dog a permanent home. We don't think it's fair for the dog or for the other families that may want to adopt.</p>",
+      ]
+    },
+    {
+      heading: "What is the process for adopting a dog?",
+      imgUrl: "http://dps-festive.imgix.net/images/about/faq/lily.jpg?h=340&fit=crop&w=340&crop=top",
+      prose: [
+        '<p>Please look at our <a href="../../adopt/dog-application/">apply page</a></p>',
+      ]
+    }
+  ]
+};
