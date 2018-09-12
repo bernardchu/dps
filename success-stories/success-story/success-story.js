9d0e82890dba82479161c24f2ac58ac5a93d53bd
bernardchu <bernardchu@gmail.com>
2018-09-11 18:55:20 -0700

    Add individual success stories.

---
+++ success-stories/success-story/success-story.js
@@ -0,0 +1,19 @@
+var id = getQueryParameterByName('id');
+
+$(document).ready(function () {
+  $.get(apis.success + "?id=" + id)
+    .then(function (response) {
+      var context = response[id];
+      context.name = id;
+      context.photos = _.flatMap(context.updates, function (update) {
+        return update.photos;
+      });
+      HandlebarsHelpers.compile(context, 'success-story', '.success-story');
+      $( '.carousel' ).slick( {
+        dots: true,
+        infinite: true,
+        speed: 300,
+        lazyLoad: 'ondemand',
+      } );
+    })
+});
