137fd8da087c10523e8154081e149c0c2d0a360b
bernardchu <bernardchu@gmail.com>
2020-04-05 15:09:45 -0700

    Generalize animal handling for dogs + anything else.

--- adopt/available/available.js
+++ adopt/available/available.js
@@ -5,19 +5,29 @@ $( document ).ready( function() {
 } );
 
 function Available () {
-  this.dogs = [];
-  this.cats = [];
+  this.firstAnimal = 'dogs'; // should be the only hard-coded animal
 }
-
+// Put all animals in their own sections with dogs first.
 Available.prototype.initialize = function () {
   var _this = this;
+  const allAnimalsContainer = $('.all-available-animals');
   return $.get( apis.available + '?view=compact' )
     .then( function ( response ) {
-      _this.dogs = response.dogs;
-      _this.cats = response.cats;
-      _this.populateAnimal( 'dogs', $( '.dogs' ) );
-      _this.populateAnimal( 'cats', $( '.cats' ) );
+      Object.keys(response).sort(function (animalName, otherAnimalName) {
+        return animalName === _this.firstAnimal ? -1 : 0;
+      }).forEach(function (animalName) {
+        if (!response[animalName] || !response[animalName].length) { return }
+        _this[animalName] = response[animalName];
+        _this.createAnimalSection(animalName, allAnimalsContainer);
+        _this.populateAnimal(animalName, $('.' + animalName));
+      });
     } );
+};
+
+Available.prototype.createAnimalSection = function ( animalName, node ) {
+  node.append('<h2>Available ' + capitalize(animalName) + '</h2>');
+  node.append('<div class="col-md-12 ' + animalName + '"></div>');
+  node.append('<div class="clearfix></div>');
 };
 
 Available.prototype.populateAnimal = function ( animal, targetNode ) {
@@ -36,3 +46,7 @@ Available.prototype.createAnimalNode = function ( animal ) {
           +'<div class="age">' + animal.age + ' - ' + animal.gender + '</div>'
         +'</span>'
 };
+
+function capitalize (s) {
+  return s.charAt(0).toUpperCase() + s.slice(1);
+}
--- print/print.js
+++ print/print.js
@@ -1,6 +1,12 @@
+// Put all animals together in a flat list with dogs first.
 var animals = [];
 $.get(apis.available + '?view=compact')
   .then(function (response) {
-    animals = response.dogs.concat(response.cats);
+    const firstAnimal = 'dogs'; // should be the only hard-coded animal
+    animals = Object.keys(response).sort(function (animalName, otherAnimalName) {
+      return animalName === firstAnimal ? -1 : 0;
+    }).reduce(function (allAnimals, animalName) {
+      return allAnimals.concat(response[animalName])
+    }, []);
     HandlebarsHelpers.compile(animals, "print", ".main");
   });
