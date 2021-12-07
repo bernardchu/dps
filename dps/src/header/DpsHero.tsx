b0dc3dcb381e9f5a6c1341904c9c6d24331688d0
Bernard Chu <bernard.chu@yahoo.com>
2021-12-07 13:52:52 -0800

    Change anchors to Links in header.

--- dps/src/header/DpsHero.tsx
+++ dps/src/header/DpsHero.tsx
@@ -1,15 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 class DpsHero extends React.Component {
   render() {
     return (
       <div className="row hero">
-        <a href="http://dpsrescue.org" className="logo col-md-2 hidden-sm hidden-xs">
+        <Link to="/" className="logo col-md-2 hidden-sm hidden-xs">
           <img src="http://dps-festive.imgix.net/images/logo-v2.png?rect=210,400,390,230&amp;auto=format" alt="DPS Logo" />
-        </a>
-        <a href="http://dpsrescue.org" className="logo hidden-md hidden-lg col-sm-10 col-xs-10">
+        </Link>
+        <Link to="/" className="logo hidden-md hidden-lg col-sm-10 col-xs-10">
           <img src="http://dps-festive.imgix.net/images/logo-v2.png?rect=210,400,690,230&amp;auto=format" alt="DPS Logo" />
-        </a>
+        </Link>
         <div className="col-md-8 col-sm-9 title hidden-sm hidden-xs">
           <h1>Doggie Protective Services</h1>
         </div>
