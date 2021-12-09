620f25778193dcfb04c253c32fce2219392b9682
Bernard Chu <bernard.chu@yahoo.com>
2021-12-08 16:29:02 -0800

    Add Twitter widget.

--- dps/src/home/DpsTwitterContainer.tsx
+++ dps/src/home/DpsTwitterContainer.tsx
@@ -1,14 +1,16 @@
 import * as React from 'react';
+import { Timeline } from 'react-twitter-widgets';
 
 export default class DpsTwitterContainer extends React.PureComponent {
   public render() {
     return (
       <div className="twitter-container card">
         <div className="twitter-embed">
-          <a className="twitter-timeline" data-width="800" data-theme="light"
-            href="https://twitter.com/DPSRescue?ref_src=twsrc%5Etfw">Tweets
-            by DPSRescue</a>
-          <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
+          <Timeline dataSource={{
+            sourceType: 'profile',
+            screenName: 'DPSRescue'
+          }}
+          />
         </div>
       </div>
     );
