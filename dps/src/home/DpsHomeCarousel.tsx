5b0340c5fb00a10c2ab9a81c83002140b883bb4c
Bernard Chu <bernard.chu@yahoo.com>
2021-12-08 20:24:19 -0800

    Explain awful type shenanigans.

--- dps/src/home/DpsHomeCarousel.tsx
+++ dps/src/home/DpsHomeCarousel.tsx
@@ -43,6 +43,12 @@ export default class DpsHomeCarousel extends React.PureComponent<{}, IDpsHomeCar
     return (
       <div className="adoptable col-md-12">
         {!loaded && <div>Loading...</div>}
+        {/* 
+        Annoying type shenanigans below because the react-slick docs incorrectly say that
+        lazyLoad should be 'progressive' or 'ondemand' when in fact it only works with a boolean.
+        Unfortunately, the @types package follows the docs, so we have to convince it to accept
+        the correct lazyLoad boolean.
+        */}
         {loaded && dogs?.length && <Slider {...DpsHomeCarousel.settings as unknown as Settings}>
           {dogs.map((dog: IDpsAdoptable) => <DpsHomeCarouselSlide dog={dog} key={dog.id} />)}
         </Slider>}
