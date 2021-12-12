3ba6e9b909a75793323670b324a4bf594418d4ad
Bernard Chu <bernard.chu@yahoo.com>
2021-12-12 11:54:10 -0800

    Fix <a> cannot be child of <a> console error.

--- dps/src/header/DpsNavTopRoute.tsx
+++ dps/src/header/DpsNavTopRoute.tsx
@@ -11,12 +11,12 @@ class DpsNavTopRoute extends React.Component<DpsNavTopRouteProps> {
   render() {
     const route: IDpsNavRoute = this.props.route;
     return (
-      <>
-        <NavLink to={route.path} className="nav-link">
+      <div className="nav-link">
+        <NavLink to={route.path} className="parent-link">
           {route.name}
-          {route.children?.length && <DpsChildRouteMenu children={route.children} parent={route} />}
         </NavLink>
-      </>
+        {route.children?.length && <DpsChildRouteMenu children={route.children} parent={route} />}
+      </div>
     );
   }
 }
