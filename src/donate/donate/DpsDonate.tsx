946315fb4aa232fb1ae2312054a15e24063f0fa3
Bernard Chu <bernard.chu@yahoo.com>
2022-01-12 10:49:34 -0800

    Reorganize main donate page.

--- src/donate/donate/DpsDonate.tsx
+++ src/donate/donate/DpsDonate.tsx
@@ -4,27 +4,33 @@ import Imgix from 'react-imgix';
 
 export default class DpsDonate extends React.PureComponent {
   public render() {
-    return (
+    return (<>
       <div className="row donate">
-        <h2>Donate</h2>
-        <div className="col-md-7 col-sm-12 prose">
-          <p>As a non-profit, we rely on community donations to continue to save dogs and support those we already have. The average cost per dog to get them adoption ready is $467, which includes bailing them out of the shelter, a health check, spay or neuter surgery, vaccinations, microchipping and spending several weeks at a foster home to evaluate before adoption.</p>
-          <p>If you would like to support our rescue, donations are always accepted. Donations can be made in the monetary form and they can be tangible items. Every penny helps.</p>
+        <div className="col-md-9 col-sm-12 prose">
+          <h2>Donate</h2>
+          As a non-profit organization with no government funding, we rely on donations to continue to rescue dogs and cats in need. The average cost per dog is $627, which includes bailing them out of the shelter, a health check, spay or neuter, vaccinations, microchipping and spending several weeks at a foster home to evaluate before adoption.  There are so many ways to donate!
+        </div>
+        <div className="col-md-3 hidden-sm hidden-xs">
+          <Imgix src="http://dps-festive.imgix.net/images/donate/donate-header.png"
+            width={300}
+            htmlAttributes={{ alt: 'Mac' }} />
+        </div>
+        <div className="col-md-6 col-sm-12 prose">
           <p><strong>Monetary Donations</strong></p>
-          <p>Besides PayPal (the Donate button in the header), you can also donate via the following: </p>
+          <p>In addition to PayPal (the Donate button in the header), you can also donate via the following:</p>
           <ul>
             <li>Venmo (@DPSRescue)</li>
             <li>Zelle (info@dpsrescue.com)</li>
             <li>Checks to PO Box 1508, Romoland, CA 92585</li>
             <li><a href="https://smile.amazon.com/hz/charitylist/ls/339V2RUI5YY50" target="_blank" rel="noreferrer">Amazon Smile</a> (A portion of your Amazon spending is donated to DPS)</li>
           </ul>
-
-
-          <p><strong>Items we need: </strong></p>
+        </div>
+        <div className="col-md-6 col-sm-12 prose">
+          <p><strong>Items we need (please contact <a href="mailto:rachel@dpsrecue.com">rachel@dpsrecue.com</a> to schedule a drop-off!)</strong></p>
           <ul>
-            <li>Canned food</li>
+            <li>Canned dog &amp; cat food of any variety</li>
             <li>Newspaper</li>
-            <li>Pens</li>
+            <li>Exercise pens</li>
             <li>Baby gates</li>
             <li>Gently used toys</li>
             <li>Blankets</li>
@@ -36,6 +42,10 @@ export default class DpsDonate extends React.PureComponent {
           <Imgix src="http://dps-festive.imgix.net/images/donate/donate-header.png" htmlAttributes={{ alt: 'Mac' }} />
         </div>
       </div>
+      <div className="row donate">
+        <h1>More Ways to Give</h1>
+      </div>
+    </>
     );
   }
 }
