23e271fd33a8703ddef9b1f5ab9dc9ad25f3ec2c
Bernard Chu <bernard.chu@yahoo.com>
2022-01-12 19:09:15 -0800

    Use Imgix for donate modal.

--- dps/src/DpsDonateModal.tsx
+++ dps/src/DpsDonateModal.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 import { routes } from './routing/routes';
+import Imgix from 'react-imgix';
 
 interface IDpsDonateModalContentProps {
   closeModal: any;
@@ -17,7 +18,16 @@ export default class DpsDonateModalContent extends React.PureComponent<IDpsDonat
         </header>
         <main className="modal__content" id="modal-1-content">
           <div className="donate-modal-image-container">
-            <img src="https://dps-festive.imgix.net/images/donate/betty-white-usa-today.jpeg?txt64=U291cmNlOiBVU0EgVG9kYXk=&txt-clr=white&w=300" alt="donate" />
+            <Imgix
+              src="https://dps-festive.imgix.net/images/donate/betty-white-usa-today.jpeg"
+              imgixParams={{
+                'txt-color': 'white',
+                // @types/react-imgix lacks foo64 param defs
+                // @ts-ignore
+                txt64: 'Source: USA Today'
+              }}
+              width={300}
+              htmlAttributes={{ alt: 'donate' }} />
           </div>
           <p>Betty White demonstrated a lifelong commitment to helping animals in need. Join us in honoring Betty White's memory to make the world a better, kinder place for all animals by making a donation in support of the work we do.</p>
           <Link onClick={() => this.props.closeModal()} to={`${routes.donate.path}/${routes.donate.children!.donate.path}`} className="hero-button col-xs-12">Donate Now</Link>
