aa8ed7cbda58322bd97d023e5f9380b87a850950
Bernard Chu <bernard.chu@yahoo.com>
2022-03-23 14:33:21 -0700

    Make loading indicator an animated ellipsis.

--- src/common/DpsLoading.tsx
+++ src/common/DpsLoading.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
+import './loading.scss';
 
 export default class DpsLoading extends React.PureComponent {
   public render() {
     return (
-      <div>
-        Loading...
-      </div>
+      // source: https://loading.io/css/ (CC0 license)
+      <div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
     );
   }
 }
