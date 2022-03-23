edb150c99c387dce6c0a7f5e65f32e4def99396d
Bernard Chu <bernard.chu@yahoo.com>
2022-03-23 14:07:40 -0700

    Don't show image on contact page for small screens.

--- src/about/contact/DpsContact.tsx
+++ src/about/contact/DpsContact.tsx
@@ -10,7 +10,7 @@ export default class DpsContact extends React.Component {
           <iframe src="https://docs.google.com/forms/d/17wNax1Hm9Ge0vxqqhGGskP_Zt6JkO4H0zqsg9MjMddA/viewform?embedded=true"
             frameBorder="0" height="600" width="640" title="Contact Form"></iframe>
         </div>
-        <div className="col-md-4">
+        <div className="col-md-4 hidden-sm hidden-xs">
           <Imgix src="https://dps-festive.imgix.net/images/about/contact/cindy.jpg" width={380} htmlAttributes={{ alt: 'Cindy', title: 'Cindy' }} />
         </div>
       </div>);
