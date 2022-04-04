6a5645123719752fd444021dc6282b3295f82a2d
dpsrescue <102438957+dpsrescue@users.noreply.github.com>
2022-04-04 15:28:05 -0700

    Update donate modal as per Caity

--- src/DpsDonateModal.tsx
+++ src/DpsDonateModal.tsx
@@ -20,11 +20,11 @@ export default class DpsDonateModalContent extends React.PureComponent<IDpsDonat
         <main className="modal__content" id="modal-1-content">
           <div className="donate-modal-image-container">
             <Imgix
-              src="/donate/donate-mama-dog.jpg"
+              src="/donate/donate-mama-dog2.jpeg"
               width={300}
               htmlAttributes={{ alt: 'donate' }} />
           </div>
-          <p>DPS currently has 7 nursing mama dogs and their 47 babies in our foster homes. Please donate to help us with veterinary care, puppy food, potty pads and the myriad other needs of these sweet rescue dogs and puppies!</p>
+          <p>DPS currently has 12 nursing mama dogs and their 52 babies in our foster homes. Please donate to help us with veterinary care, puppy food, potty pads and the myriad other needs of these sweet rescue dogs and puppies!</p>
           <Link onClick={() => this.props.closeModal()} to={`${routes.donate.path}/${routes.donate.children!.donate.path}`} className="hero-button col-xs-12">Donate Now</Link>
         </main>
       </>
