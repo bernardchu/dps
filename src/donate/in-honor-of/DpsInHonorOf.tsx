242176611254dfbb7ea7ccacbd284699e06e2f93
Bernard Chu <bernard.chu@yahoo.com>
2022-04-04 17:33:52 -0700

    Add images to in honor of and sponsor pages.

--- src/donate/in-honor-of/DpsInHonorOf.tsx
+++ src/donate/in-honor-of/DpsInHonorOf.tsx
@@ -1,17 +1,24 @@
 import * as React from 'react';
+import Imgix from 'react-imgix';
+import './inHonorOf.scss';
 
 export default class DpsInHonorOf extends React.PureComponent {
   public render() {
     return (
-      <>
-        <h1>In Honor Of</h1>
-        <p>Honor a human or furry loved one with a tribute to them.</p>
-        <p>
-          Step 1: Submit your donation through Venmo (@DPSRescue) or Zelle (info@dpsrescue.com)<br />
-          Step 2: Email <a href="mailto:rachel@dpsrescue.com">rachel@dpsrescue.com</a> with a photo (if you’d like to submit one) and a message (ex: In memory of my beloved Fluffy, adopted in 2002)
-        </p>
-        <p>Your photo and message can be posted on our social media, a page of our website or we can send an individual note. You get to choose how you’d like your pet to be remembered. We know the importance of pets and how essential it is to celebrate both our pets and the humans in our lives.</p>
-      </>
+      <div className="row in-honor-of">
+        <div className="col-md-8 col-sm-12">
+          <h1>In Honor Of</h1>
+          <p>Honor a human or furry loved one with a tribute to them.</p>
+          <p>
+            Step 1: Submit your donation through Venmo (@DPSRescue) or Zelle (info@dpsrescue.com)<br />
+            Step 2: Email <a href="mailto:rachel@dpsrescue.com">rachel@dpsrescue.com</a> with a photo (if you’d like to submit one) and a message (ex: In memory of my beloved Fluffy, adopted in 2002)
+          </p>
+          <p>Your photo and message can be posted on our social media, a page of our website or we can send an individual note. You get to choose how you’d like your pet to be remembered. We know the importance of pets and how essential it is to celebrate both our pets and the humans in our lives.</p>
+        </div>
+        <div className="col-md-4 hidden-sm hidden-xs image">
+          <Imgix src="donate/in-honor-of/Yoda.png" htmlAttributes={{ alt: 'Yoda 2001-2020', title: 'Yoda 2001-2020' }} />
+        </div>
+      </div>
     );
   }
 }
--- src/donate/sponsor/DpsSponsor.tsx
+++ src/donate/sponsor/DpsSponsor.tsx
@@ -1,17 +1,23 @@
 import * as React from 'react';
 import './sponsor.scss';
+import Imgix from 'react-imgix';
 
 export default class DpsSponsor extends React.PureComponent {
   public render() {
     return (
-      <div className="sponsor">
-        <h1>Sponsor a Dog</h1>
-        <em>Your sponsorship helps pay for this dog's spay/neuter surgery, flea treatment, food, and more!</em>
-        <p>Step 1: Visit our Adoptable Dogs page and choose a dog you would like to sponsor</p>
-        <p>Step 2: Submit a $50 donation via Zelle or Venmo</p>
-        <p>Step 3: Send an email to <a href="mailto:rachel@dpsrescue.com">rachel@dpsrescue.com</a> with the name of the dog you’d like to sponsor and a short message (ex: Sponsored by Sarah in honor of her dog Rex)</p>
-        <br />
-        <p>Your message will be posted on the dog’s bio within 48 hours!</p>
+      <div className="sponsor donate row">
+        <div className="col-md-8 col-sm-12">
+          <h1>Sponsor a Dog</h1>
+          <em>Your sponsorship helps pay for this dog's spay/neuter surgery, flea treatment, food, and more!</em>
+          <p>Step 1: Visit our Adoptable Dogs page and choose a dog you would like to sponsor</p>
+          <p>Step 2: Submit a $50 donation via Zelle or Venmo</p>
+          <p>Step 3: Send an email to <a href="mailto:rachel@dpsrescue.com">rachel@dpsrescue.com</a> with the name of the dog you’d like to sponsor and a short message (ex: Sponsored by Sarah in honor of her dog Rex)</p>
+          <br />
+          <p>Your message will be posted on the dog's bio within 48 hours!</p>
+        </div>
+        <div className="col-md-4 hidden-sm hidden-xs image">
+          <Imgix src="donate/sponsor/Nebula.png" htmlAttributes={{ alt: 'Nebula\'s care was sponsored by Ima Lifesaver', title: 'Nebula example' }} />
+        </div>
       </div>
     );
   }
