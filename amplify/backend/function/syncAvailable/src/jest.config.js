b1146c978516c34ad43f09a73569ef302179737f
Bernard Chu <bernard.chu@yahoo.com>
2022-04-07 20:02:38 -0700

    Oops don't gitignore jest config.

---
+++ amplify/backend/function/syncAvailable/src/jest.config.js
@@ -0,0 +1,10 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: [
+    "../src/test"
+  ],
+  verbose: true,
+  modulePathIgnorePatterns: ['.js']
+};
