f8f805fa92213f188128777e0fcf270f6e93f1bc
Bernard Chu <bernard.chu@yahoo.com>
2022-05-06 14:38:03 -0700

    Add dog/cat application redirect pages.

---
+++ pages/adopt/application.tsx
@@ -0,0 +1,5 @@
+import DpsRedirect from "../../src/routing/DpsRedirect";
+
+export default function ApplicationRedirect() {
+  return <DpsRedirect to="https://docs.google.com/forms/d/e/1FAIpQLSf3PdbO6nBVx03MbUBRp-mr-j0u46eVkVD1_m8mTeI8hR98tg/viewform" />
+}
---
+++ pages/adopt/catapp.tsx
@@ -0,0 +1,5 @@
+import DpsRedirect from "../../src/routing/DpsRedirect";
+
+export default function CatAppRedirect() {
+  return <DpsRedirect to="https://docs.google.com/forms/d/e/1FAIpQLSfLhSWqJWHMrW8cYu5Mf6yruf6rXPWG9iPNmibouDUFFVlZ-A/viewform" />
+}
