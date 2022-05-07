e8719294fc19c9c9b502c06fc4582b774a856bc8
Bernard Chu <bernard.chu@yahoo.com>
2022-05-07 13:49:43 -0700

    Add donate page.

---
+++ pages/donate/donate.tsx
@@ -0,0 +1,5 @@
+import DpsDonate from "../../src/donate/donate/DpsDonate"
+
+export default function Donate() {
+  return <DpsDonate />
+}
--- src/donate/donate/DpsDonate.tsx
+++ src/donate/donate/DpsDonate.tsx
@@ -15,8 +15,8 @@ export default class DpsDonate extends React.PureComponent {
     const otherDonateRoutes: IDpsRoute[] = Object.keys(donateRoutes).map(key => donateRoutes[key]).filter(route => !DpsDonate.isDonateRoute(route));
 
     return (<>
-      <div className="row donate">
-        <div className="col-md-9 col-sm-12 prose">
+      <div className={`row ${styles.donate}`}>
+        <div className={`col-md-9 col-sm-12 ${styles.prose}`}>
           <h2>Donate</h2>
           As a non-profit organization with no government funding, we rely on donations to continue to rescue dogs and cats in need. The average cost per dog is $627, which includes bailing them out of the shelter, a health check, spay or neuter, vaccinations, microchipping and spending several weeks at a foster home to evaluate before adoption.  There are so many ways to donate!
         </div>
@@ -26,7 +26,7 @@ export default class DpsDonate extends React.PureComponent {
             className="no-border"
             htmlAttributes={{ alt: 'Mac' }} />
         </div>
-        <div className="col-md-6 col-sm-12 prose">
+        <div className={`col-md-6 col-sm-12 ${styles.prose}`}>
           <p><strong>Monetary Donations</strong></p>
           <p>In addition to PayPal (the Donate button in the header), you can also donate via the following:</p>
           <ul>
@@ -36,7 +36,7 @@ export default class DpsDonate extends React.PureComponent {
             <li><a href="https://smile.amazon.com/hz/charitylist/ls/339V2RUI5YY50" target="_blank" rel="noreferrer">Amazon Smile</a> (A portion of your Amazon spending is donated to DPS)</li>
           </ul>
         </div>
-        <div className="col-md-6 col-sm-12 prose">
+        <div className={`col-md-6 col-sm-12 ${styles.prose}`}>
           <p><strong>Items we need (please contact <a href="mailto:rachel@dpsrescue.com">rachel@dpsrescue.com</a> to schedule a drop-off!)</strong></p>
           <ul>
             <li>Canned dog &amp; cat food of any variety</li>
@@ -52,11 +52,11 @@ export default class DpsDonate extends React.PureComponent {
         </div>
       </div>
       <hr />
-      <div className="row donate">
+      <div className={`row ${styles.donate}`}>
         <h1>More Ways to Give</h1>
-        <div className="sibling-tiles">
+        <div className={styles['sibling-tiles']}>
           {otherDonateRoutes.map(route => <Link key={route.name} href={`../${route.path}`}>
-            <a className="donate-sibling-tile col-xs-6 col-md-3" >
+            <a className={`${styles['donate-sibling-tile']} col-xs-6 col-md-3`} >
               <Imgix src="/donate/donate-paw-print.png" width={300} />
               {route.name}
             </a>
