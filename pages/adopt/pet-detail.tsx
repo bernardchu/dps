ef920d87c854017fd90f30aafc920120f275edd8
Bernard Chu <bernard.chu@yahoo.com>
2022-05-08 11:09:03 -0700

    Add twitter meta tags.

--- pages/adopt/pet-detail.tsx
+++ pages/adopt/pet-detail.tsx
@@ -5,16 +5,28 @@ import { IDpsAvailableIdResponse } from "../../src/api/IDpsApiResponses";
 import { pageTitle } from "../../src/common/DpsConstants";
 
 export default function PetDetail({ pet, print, host }: { pet: IDpsAvailableIdResponse, print: boolean, host: string }) {
+  const title = `${pageTitle} - ${pet.name}`;
+  const description = `${pet.age} ${pet.breed} ${pet.species} available for adoption from ${pageTitle}`;
+  const imageAlt = `A photo of ${pet.name}`;
   return <>
     <Head>
-      <meta property="og:title" content={`${pageTitle} - ${pet.name}`} />
+      {/* OpenGraph */}
+      <meta property="og:title" content={title} />
       <meta property="og:type" content="website" />
       {host && <meta property="og:url" content={host} />}
       {pet.pictures.map(picture => <meta property="og:image" content={picture} key={picture} />)}
-      <meta property="og:image:alt" content={`A photo of ${pet.name}`} />
-      <meta property="og:description" content={`${pet.age} ${pet.breed} ${pet.species} available for adoption from ${pageTitle}`} />
+      <meta property="og:image:alt" content={imageAlt} />
+      <meta property="og:description" content={description} />
       <meta property="og:site_name" content={pageTitle} />
       {pet.video && <meta property="og:video" content={pet.video} />}
+
+      {/* Twitter */}
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={pet.pictures[0]} />
+      <meta name="twitter:image:alt" content={imageAlt} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@DPSRescue" />
     </Head>
     <DpsPetDetail pet={pet} print={print} />
   </>
