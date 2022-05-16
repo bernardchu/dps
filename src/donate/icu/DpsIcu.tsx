020fa5d533205decf10f0e50f7b627e3e42ce1ab
Bernard Chu <bernard.chu@yahoo.com>
2022-05-16 14:50:40 -0700

    Fix react key warning.

--- src/donate/icu/DpsIcu.tsx
+++ src/donate/icu/DpsIcu.tsx
@@ -32,7 +32,7 @@ export default function DpsIcu(props: IDpsIcuProps) {
       <h2>Medical Dogs</h2>
       <p>In 2021, we spent over $100,000 on medical dogs. We hope to save even more dogs with medical special needs in 2022, but we need your help! Click on each dog to read their story. You can donate via the link on their description or via Zelle, Venmo, or by sending a check.</p>
       <div className={`col-xs-12 row ${styles.icu}`}>
-        {icu.map(animal => <DpsIcuTile animal={animal} handleOpen={handleOpen.bind(null, animal)} />)}
+        {icu.map(animal => <DpsIcuTile animal={animal} handleOpen={handleOpen.bind(null, animal)} key={animal.photo} />)}
         <Modal
           isOpen={modalIsOpen}
           onRequestClose={closeModal}
