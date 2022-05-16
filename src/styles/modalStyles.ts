c2ddc127cdfc9622795be2f5b428df6eae064c30
Bernard Chu <bernard.chu@yahoo.com>
2022-05-16 14:50:09 -0700

    Restrict modal height, ensure hamburger is under modal overlay.

--- src/styles/modalStyles.ts
+++ src/styles/modalStyles.ts
@@ -7,6 +7,10 @@ export const modalStyles = {
     marginRight: '-50%',
     transform: 'translate(-50%, -50%)',
     maxWidth: '80vw',
-    width: '500px'
+    width: '500px',
+    maxHeight: '70vh'
+  },
+  overlay: {
+    zIndex: '1001' // necessary to overcome 1000 set by default on hamburger
   }
 };
