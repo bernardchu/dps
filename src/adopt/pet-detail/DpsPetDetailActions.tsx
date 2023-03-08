f785fb926c2e09cad640744855a7bbc2dd52937f
Bernard Chu <bernard.chu@yahoo.com>
2023-03-08 14:32:29 -0800

    Make code more readable.

--- src/adopt/pet-detail/DpsPetDetailActions.tsx
+++ src/adopt/pet-detail/DpsPetDetailActions.tsx
@@ -12,8 +12,16 @@ export default class DpsPetDetailActions extends React.PureComponent<IDpsPetDeta
     const pet = this.props.pet;
     return (
       <div className="pet-detail-buttons row">
-        <span className="application-link col-xs-12 col-md-6"><Link to={pet.isDog ? `../${routes.adopt.children!.dogApp.path}` : `../${routes.adopt.children!.catApp.path}`}>Apply to adopt</Link></span>
-        <span className="available-link col-xs-12 col-md-6"><Link to={`../${routes.adopt.children!.available.path}`}>See more {pet.species.toLocaleLowerCase()}s</Link></span>
+        <span className="application-link col-xs-12 col-md-6">
+          <Link to={pet.isDog ? `../${routes.adopt.children!.dogApp.path}` : `../${routes.adopt.children!.catApp.path}`}>
+            Apply to adopt
+          </Link>
+        </span>
+        <span className="available-link col-xs-12 col-md-6">
+          <Link to={`../${routes.adopt.children!.available.path}`}>
+            See more {pet.species.toLocaleLowerCase()}s
+          </Link>
+        </span>
       </div>
     );
   }
