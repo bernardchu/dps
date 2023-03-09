3065cdd9e918c835a758fb926e2eef707c43a616
Bernard Chu <bernard.chu@yahoo.com>
2023-03-09 10:17:31 -0800

    Make banner responsive.

--- src/adopt/available/DpsSocialBanner.tsx
+++ src/adopt/available/DpsSocialBanner.tsx
@@ -1,46 +1,75 @@
 import * as React from 'react';
 import Imgix from 'react-imgix';
-import { Link } from 'react-router-dom';
-import DpsDetailedPet from '../../model/DpsDetailedPet';
-import { routes } from '../../routing/routes';
 
 export default class DpsSocialBanner extends React.PureComponent {
-  private static SOCIAL_ICON_WIDTH = 60; // px
-
   public render() {
     return (
       <div className="social-banner-container">
-        <div className="social-banner">
-          <Imgix
-            src="/Follow-us.png"
-            width={100}
-            className="no-border hidden-xs"
-            htmlAttributes={{ alt: 'Follow us' }} />
+        <div className="social-banner hidden-xs">
+          <FollowUs />
           <div className="social-banner-text">
-            <h2>Don&rsquo;t see the perfect pup?</h2>
-            <h4>Be the first to find out when new dogs and puppies are available!</h4>
+            <BannerText />
           </div>
-          <div className="image-container hidden-xs">
-
-            <a href="https://www.facebook.com/DPSRescue" target="_blank" rel="noreferrer">
-              <Imgix
-                className="social-icon"
-                src="/social-icons-facebook.png"
-                width={DpsSocialBanner.SOCIAL_ICON_WIDTH}
-                htmlAttributes={{ alt: 'DPSRescue Facebook' }} />
-            </a>
-            <a href="https://www.instagram.com/dpsrescue/" target="_blank" rel="noreferrer">
-              <Imgix
-                className="social-icon"
-                src="/social-icons-instagram.png"
-                width={DpsSocialBanner.SOCIAL_ICON_WIDTH}
-                imgixParams={{ pad: 7, bg: 'cd486b' }}
-                htmlAttributes={{ alt: 'DPSRescue Instagram' }} />
-            </a>
-
+          <div className="image-container ">
+            <SocialIcons />
+          </div>
+        </div>
+        <div className="social-banner social-banner-xs hidden-sm hidden-md hidden-lg hidden-xl">
+          <div className="social-banner-text">
+            <BannerText />
+          </div>
+          <div className="image-container ">
+            <FollowUs />
+            <div className="social-icons">
+              <SocialIcons />
+            </div>
           </div>
         </div>
       </div>
     );
+  }
+}
+
+class FollowUs extends React.PureComponent {
+  public render() {
+    return (
+      <Imgix
+        src="/Follow-us.png"
+        width={100}
+        className="no-border"
+        htmlAttributes={{ alt: 'Follow us' }} />
+    );
+  }
+}
+
+class BannerText extends React.PureComponent {
+  public render() {
+    return (<>
+      <h2>Don&rsquo;t see the perfect pup?</h2>
+      <h4>Be the first to find out when new dogs and puppies are available!</h4>
+    </>);
+  }
+}
+
+class SocialIcons extends React.PureComponent {
+  private static SOCIAL_ICON_WIDTH = 60; // px
+  public render() {
+    return (<>
+      <a href="https://www.facebook.com/DPSRescue" target="_blank" rel="noreferrer">
+        <Imgix
+          className="social-icon"
+          src="/social-icons-facebook.png"
+          width={SocialIcons.SOCIAL_ICON_WIDTH}
+          htmlAttributes={{ alt: 'DPSRescue Facebook' }} />
+      </a>
+      <a href="https://www.instagram.com/dpsrescue/" target="_blank" rel="noreferrer">
+        <Imgix
+          className="social-icon"
+          src="/social-icons-instagram.png"
+          width={SocialIcons.SOCIAL_ICON_WIDTH}
+          imgixParams={{ pad: 7, bg: 'cd486b' }}
+          htmlAttributes={{ alt: 'DPSRescue Instagram' }} />
+      </a>
+    </>);
   }
 }
