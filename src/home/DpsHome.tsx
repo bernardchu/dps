a30a98ef87589eab64d83307137588ab600a7531
Bernard Chu <bernard.chu@yahoo.com>
2024-03-13 18:01:11 -0700

    Revert "Move giving tuesday feature to top."
    This reverts commit e009294a8992a1c0c84c4e167ac757d16ab4a815.

--- src/home/DpsHome.tsx
+++ src/home/DpsHome.tsx
@@ -87,11 +87,11 @@ export default class DpsHome extends React.PureComponent<{}, IDpsHomeState> {
           </div>
         </div>
         <div className="col-md-8 col-sm-12 row features">
-          <DpsHomeFeatureCard feature={givingTuesdayFeature} />
           <DpsHomeEvents />
           {!loaded && <DpsLoading />}
           {loaded && featured.map(feature => <DpsHomeFeatureCard feature={feature} />)}
           <DpsHomeFeatureCard feature={merchFeature} />
+          <DpsHomeFeatureCard feature={givingTuesdayFeature} />
           <div className="col-md-12 featured card">
             {/* <!-- Begin Constant Contact Inline Form Code --> */}
             <div className="ctct-inline-form" data-form-id="507eddd4-ebbf-4431-b7d1-e68dc4c14e4f"></div>
