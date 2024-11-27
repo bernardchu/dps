90ddd815525db7fa29a7779062ac354687e19239
Bernard Chu <bernard.chu@yahoo.com>
2024-11-26 19:16:26 -0800

    Update donate button link.

--- src/common/DpsConstants.ts
+++ src/common/DpsConstants.ts
@@ -1,4 +1,4 @@
 export const paypalEmail = 'info@dpsrescue.com';
 export const donateEmail = 'info@dpsrescue.com';
-export const donateLink = 'https://bit.ly/3Tkw2bD';
+export const donateLink = 'https://www.zeffy.com/peer-to-peer/support-doggie-protective-services-for-homeless-tails-dps-rescue-this-giving-tuesday';
 export const paypalLink = 'https://www.paypal.com/donate/?hosted_button_id=JYKGZV458ZS2N#';
