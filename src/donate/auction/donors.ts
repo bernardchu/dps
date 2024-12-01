09fbd9b12c77ec51b08eff2d0f97728baa07b197
Cindy Chu <9750894+roastonbone@users.noreply.github.com>
2024-12-01 08:31:38 -0800

    Add donors

--- src/donate/auction/donors.ts
+++ src/donate/auction/donors.ts
@@ -5,5 +5,65 @@ export const DONORS: IDpsAuctionDonor[] = [
     name: 'US Ghost Adventures',
     link: 'https://usghostadventures.com/',
     imgSrc: 'donate/auction/usga.jpeg'
-  }
+  },
+  {
+    name: 'Malibu Wine Hikes',
+    link: 'https://www.malibuwinehikes.com/',
+    imgSrc: 'donate/auction/malibu-wine.jpg'
+  },
+  {
+    name: 'Hang Smart',
+    link: 'https://hangsmart.com/',
+    imgSrc: 'donate/auction/hang-smart.webp'
+  },
+  {
+    name: 'The Huntington',
+    link: 'https://huntington.org/',
+    imgSrc: 'donate/auction/huntington.svg'
+  },
+  {
+    name: 'Aqui',
+    link: 'https://www.aquicalmex.com/',
+    imgSrc: 'donate/auction/aqui.png'
+  },
+  {
+    name: 'Winchester Mystery House',
+    link: 'https://winchestermysteryhouse.com/',
+    imgSrc: 'donate/auction/winchester.webp'
+  },
+  {
+    name: 'Santa Barbara Zoo',
+    link: 'https://www.sbzoo.org/',
+    imgSrc: 'donate/auction/sb-zoo.webp'
+  },
+  {
+    name: 'The Walt Disney Family Museum',
+    link: 'https://www.waltdisney.org/',
+    imgSrc: 'donate/auction/wdfm.svg'
+  },
+  {
+    name: 'Oakland Zoo',
+    link: 'https://www.oaklandzoo.org/',
+    imgSrc: 'donate/auction/oakland-zoo.svg'
+  },
+  {
+    name: 'The Escape Game',
+    link: 'https://theescapegame.com/',
+    imgSrc: 'donate/auction/escape-game.svg'
+  },
+  {
+    name: 'Dream Inn',
+    link: 'https://www.dreaminnsantacruz.com/',
+    imgSrc: 'donate/auction/dream-inn.png'
+  },
+  {
+    name: 'Gourmet Gift Baskets',
+    link: 'https://www.gourmetgiftbaskets.com/',
+    imgSrc: 'donate/auction/gourmet-gift-baskets.webp'
+  }/*,
+  {
+    name: '',
+    link: '',
+    imgSrc: 'donate/auction/'
+  }*/
 ];
