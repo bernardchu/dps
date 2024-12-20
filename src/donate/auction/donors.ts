accd2b8f66d73a436592bf9313c6f2e25db29798
dpsrescue <102438957+dpsrescue@users.noreply.github.com>
2024-12-20 14:19:17 -0800

    Update donors.ts
    revert one-off padding change

--- src/donate/auction/donors.ts
+++ src/donate/auction/donors.ts
@@ -117,7 +117,7 @@ export const DONORS: IDpsAuctionDonor[] = [
   },{
     name: 'Barry\'s Bootcamp Palo Alto',
     link: 'https://www.barrys.com/studio/palo-alto/',
-    imgSrc: 'barrys.svg?bg=black&pad=10'
+    imgSrc: 'barrys.svg?bg=black'
   },{
     name: 'Charlee Bear',
     link: 'https://charleebear.com/',
