0a08f6f309b3396d3e2d33d7d308c52f29b4785f
Bernard Chu <bernard.chu@yahoo.com>
2025-02-16 21:41:42 -0800

    Update donate link.

--- src/common/DpsConstants.ts
+++ src/common/DpsConstants.ts
@@ -1,4 +1,4 @@
 export const paypalEmail = 'info@dpsrescue.com';
 export const donateEmail = 'info@dpsrescue.com';
-export const donateLink = 'https://www.zeffy.com/donation-form/donate-to-make-a-difference-4232';
+export const donateLink = 'https://www.zeffy.com/donation-form/donate-to-help-save-lives-2';
 export const paypalLink = 'https://www.paypal.com/donate/?hosted_button_id=JYKGZV458ZS2N#';
