0bda1014a3be3946d6510a7cf81bf1bba6769fe3
Cindy Chu <9750894+roastonbone@users.noreply.github.com>
2025-02-18 20:20:30 -0800

    Update donors.ts

--- src/donate/auction/donors.ts
+++ src/donate/auction/donors.ts
@@ -291,10 +291,6 @@ export const DONORS: IDpsAuctionDonor[] = [
     link: 'https://www.wondercide.com/',
     imgSrc: 'wondercide.avif'
   }, {
-    name: 'The Lawrence Hall of Science',
-    link: 'https://lawrencehallofscience.org/',
-    imgSrc: 'lawrence.png?bg=black'
-  }, {
     name: 'San Jose Brew Bike',
     link: 'https://www.sanjosebrewbike.com/',
     imgSrc: 'sj-brew-bike.png'
